2f351ad6b0cb49e7dd180d0ac89eb057dfd7d9dd
Tomas Tomecek <ttomecek@redhat.com>
2015-09-24 16:44:45 +0200

    init

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM fedora
+RUN rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm
 ADD fedora-spotify.repo /etc/yum.repos.d/
-RUN dnf update -y && dnf install -y spotify-client && dnf clean all
+RUN dnf update -y && \
+    dnf install -y spotify-client strace xorg-x11-server-utils mesa-dri-drivers && \
+    dnf clean all
 
+CMD spotify
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker run \
+    --net host \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=/.Xauthority \
+    -v ~/.Xauthority:/.Xauthority:ro \
+    --device /dev/snd \
+    --device /dev/dri \
+    --name spotify \
+    --privileged --rm -ti\
+    spotify bash
+
+#    -v /etc/localtime:/etc/localtime:ro \
+#    -v /run:/run \
+#    -v /dev/shm:/dev/shm \
+#    -v /etc/machine-id:/etc/machine-id \
+#    -e DBUS_SESSION_BUS_ADDRESS=/run/user/1000/dbus/user_bus_socket \
